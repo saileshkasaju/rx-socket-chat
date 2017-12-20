d09c41ecd18282b88137b7013eec6e36bbb02547
saileshkasaju <leosailesh@gmail.com>
2017-12-20 22:36:32 +0545

    add and remove users on socket connection and disconnection using rxjs

--- rx-socket-chat-server/index.js
+++ rx-socket-chat-server/index.js
@@ -1,7 +1,11 @@
+import { userInfo } from "os";
+
 const express = require("express");
 const http = require("http");
 const bodyParser = require("body-parser");
 const socket = require("socket.io");
+const Immutable = require("immutable");
+const { Observable } = require("rxjs");
 
 const app = express();
 
@@ -11,7 +15,26 @@ const server = http.createServer(app);
 
 const io = socket(server);
 
+const userList = Immutable.Map({});
+
 // socket logic on connect
+const sourceConnect = Observable.create(observer => {
+  io.on("connection", socket => {
+    socket.emit("my socketId", {
+      socketId: socket.id,
+      connectTime: Date.now()
+    });
+  });
+
+  socket.on("client connect", data => {
+    observer.next({ socket: socket, data: data, event: "client connect" });
+  });
+});
+
+sourceConnect.subscribe(obj => {
+  userList.set(obj.data.socketId, obj.data);
+  io.emit("all users", userList.toArray());
+});
 
 // socket logic for post message
 app.post("/message", (req, res) => {
@@ -20,6 +43,19 @@ app.post("/message", (req, res) => {
 });
 
 // socket logic for disconnect
+
+const sourceDisconnect = Observable.create(observer => {
+  io.on("connection", socket => {
+    socket.on("disconnect", data => {
+      observer.next({ socketId: socket.id, event: "client disconnect" });
+    });
+  });
+});
+
+sourceDisconnect.subscribe(obj => {
+  userList.delete(obj.socketId);
+  io.emit("all users", userList.toArray());
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
